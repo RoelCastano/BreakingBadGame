97ec096f630b090a1ec0129978781ae855339cb9
RoelCastano <roelcastanomoreno@gmail.com>
2014-03-08 17:23:29 -0600

    con metodos de JFrame

--- src/breakingbadgame/JFrameBreakingBadGame.java
+++ src/breakingbadgame/JFrameBreakingBadGame.java
@@ -6,10 +6,34 @@
 
 package breakingbadgame;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+
 /**
  *
  * @author roelcastano
  */
-public class JFrameBreakingBadGame {
+public class JFrameBreakingBadGame extends JFrame implements Runnable, KeyListener {
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
