fbde405f9ab3c5a73f41f2b361eb6ccd408b4599
RoelCastano <roelcastanomoreno@gmail.com>
2014-03-08 17:17:46 -0600

    first files

--- src/breakingbadgame/BreakingBadGame.java
+++ src/breakingbadgame/BreakingBadGame.java
@@ -17,6 +17,9 @@ public class BreakingBadGame {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JFrameBreakingBadGame juego = new JFrameBreakingBadGame(); //Crea una variable de JFrameProjectX 
+        juego.setVisible(true); //Hace que se vea el jframe
+        juego.setSize(900, 700); //Ajusta el tamaño inicial del jframe        
     }
     
 }
